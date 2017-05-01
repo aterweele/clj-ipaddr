f555fb884add36bd459a44e8e6c549fa6909219f
Alex ter Weele <alexterweele@centripetalnetworks.com>
2017-05-01 10:24:04 -0400

    (squash) convert to protocol, remove number-based constructors

--- src/clj_ipaddr/core.clj
+++ src/clj_ipaddr/core.clj
@@ -1,61 +1,31 @@
 (ns clj-ipaddr.core
+  ;; TODO: clean up any unused imports
   (:import [clojure.lang BigInt]
            [com.google.common.primitives Ints]
            [java.math BigInteger]
-           [java.net InetAddress]))
+           [java.net InetAddress Inet4Address Inet6Address]))
 
-;; TODO: the dispatch is purely on type, so a defprotocol and
-;; extend-protocol might pattern be a better fit here.
-(defmulti ip-address
-  "Yields a java.net.InetAddress of the provided value."
-  class)
-(defmethod ip-address String [s]
-  (InetAddress/getByName s))
-(defmethod ip-address
-  ;; byte array
-  (Class/forName "[B") [bs]
-  (InetAddress/getByAddress bs))
-(defmethod ip-address Integer [i]
-  (-> i Ints/toByteArray ip-address))
-;; TODO: this is very opinionated. Anything bigger than an Integer is
-;; too long to be an IPv4 address and is therefore made into an IPv6
-;; address. OTOH I do not want to ignore the information in the
-;; types. Another approach might do that.
-(defmethod ip-address BigInteger [i]
-  (let [ipv6-addr-size 16
-        bs (.toByteArray i)
-        l (count bs)]
-    ;; FIXME: this 0-padding scheme is janky and probably does
-    ;; something unexpected with numbers bigger than 16
-    ;; bytes (expected behavior for >16 bytes: make a byte array that
-    ;; is too long and fail when trying to make an IPv6 address out of
-    ;; that.)
+(defprotocol IPAddressRepresentation
+  (ip-address [this] "Yields a java.net.InetAddress of the provided value."))
 
-    ;; I am guessing that the actual behavior on such numbers will be
-    ;; that they are truncated to a multiple of 20 bytes and are not
-    ;; padded.
+;; byte array. Does not work inside the extend-protocol below.
+(extend-type (Class/forName "[B")
+  IPAddressRepresentation
+  (ip-address [bs] (InetAddress/getByAddress bs)))
 
-    ;; thinking about it more though I think the behavior on big
-    ;; numbers might be right.
-    (-> (partition ipv6-addr-size
-                   ipv6-addr-size
-                   bs
-                   (repeat (- ipv6-addr-size l) 0))
-        ;; tbh this is the jankiest bit. the fact that this is trying
-        ;; to be a pad, but that it has to take this extra flattening
-        ;; step...no good
-        flatten
-        byte-array
-        ip-address)))
-(defmethod ip-address BigInt [i]
-  ;; TODO: I think it is right but I cannot find the docs for BigInt
-  ;; so I am not sure of the semantics of .toBigInteger
+(extend-protocol IPAddressRepresentation
+  String
+  (ip-address [s] (InetAddress/getByName s)))
 
-  ;; avoidable by making BigInt be in charge of the padding (see
-  ;; above) and BigInteger defer to BigInt.
-  (-> i .toBigInteger ip-address))
-(defmethod ip-address Long [l]
-  (-> l bigint ip-address))
+;; TODO: consider removing these two. Cute but perhaps not useful.
+(defprotocol IPAddress
+  (version [this]))
+
+(extend-protocol IPAddress
+  Inet4Address
+  (version [_] #_::ipv4 4)
+  Inet6Address
+  (version [_] #_::ipv6 6))
 
 ;; TODO: write the network-making stuff.
 
